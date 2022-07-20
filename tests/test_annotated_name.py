c226fea1f90ec9e52a173cd83ba52251ec19ed93
mvaradi <mvaradi@ebi.ac.uk>
2022-07-20 17:27:18 +0100

    Restoring unit tests for get_annotated_name.py

--- tests/test_annotated_name.py
+++ tests/test_annotated_name.py
@@ -1,78 +1,9 @@
-from complexes.utils.get_annotated_name import GetAnnotatedName
 from unittest import TestCase
 from unittest.mock import patch
+import os
 
+from complexes.utils.get_annotated_name import GetAnnotatedName
 
-expected_result_one = [
-    {
-        "accession": "P43773",
-        "stoichiometry": "12",
-        "accession_stoichiometry": "P43773_12",
-    },
-    {
-        "accession": "P43772",
-        "stoichiometry": "12",
-        "accession_stoichiometry": "P43772_12",
-    },
-]
-
-expected_result_two = [
-    {
-        "accession": "P19483",
-        "stoichiometry": "3",
-        "accession_stoichiometry": "P19483_3",
-    },
-    {
-        "accession": "P00829",
-        "stoichiometry": "3",
-        "accession_stoichiometry": "P00829_3",
-    },
-    {
-        "accession": "P05631",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P05631_1",
-    },
-    {
-        "accession": "P05630",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P05630_1",
-    },
-    {
-        "accession": "P05632",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P05632_1",
-    },
-    {
-        "accession": "P32876",
-        "stoichiometry": "8",
-        "accession_stoichiometry": "P32876_8",
-    },
-    {
-        "accession": "P13621",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P13621_1",
-    },
-    {
-        "accession": "P13619",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P13619_1",
-    },
-    {
-        "accession": "P13620",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P13620_1",
-    },
-    {
-        "accession": "P02721",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P02721_1",
-    },
-    {
-        "accession": "P00847",
-        "stoichiometry": "1",
-        "accession_stoichiometry": "P00847_1",
-    },
-]
 
 stub_data = [
     {
@@ -92,24 +23,32 @@ class TestGetAnnotatedName(TestCase):
     # TODO Convert all the other tests to this format
 
     def setUp(self) -> None:
-        # self.gan = GetAnnotatedName()
-        # self.gan.get_data()
-        self.molecule_name_path = "mock_molecule_name.csv"
-        self.molecule_components_path = "molecule_components_path"
+        self.gan = GetAnnotatedName(
+            os.path.join('tests', 'data', 'complexes_molecules.csv'),
+            os.path.join('tests', 'data', 'complexes_components.csv'))
 
-    # def test_read_molecule_names(self):
-    #     # Test if method returns the correct name
-    #     self.assertEqual("AAA+ proteases", self.gan.molecule_names["1"])
-    #     self.assertEqual("ATP Synthase", self.gan.molecule_names["27"])
+    def test_get_data(self):
+        self.gan.get_data()
 
-    # def test_read_components(self):
-    #     # Test if method returns the correct components for a complex
-    #     self.assertEqual(expected_result_one, self.gan.molecule_components["1"])
-    #     self.assertEqual(expected_result_two, self.gan.molecule_components["27"])
+        # Test if method returns the correct name
+        self.assertEqual("AAA+ proteases",
+                         self.gan.molecule_names["1"])
+        self.assertEqual("ABO Blood Type Glycosyltransferases",
+                         self.gan.molecule_names["2"])
 
-    @patch("complexes.utils.get_annotated_name.GetAnnotatedName.read_molecule_names")
-    def test_read_molecule_names(self, cb):
-        an = GetAnnotatedName(self.molecule_name_path, self.molecule_components_path)
-        cb.data.return_value = stub_data
-        an._read_molecule_names()
-        print(an.molecule_names)
+        # Test if method returns the correct components name
+        expected_result = [
+            {
+                "accession": "P43773",
+                "stoichiometry": "12",
+                "accession_stoichiometry": "P43773_12",
+            },
+            {
+                "accession": "P43772",
+                "stoichiometry": "12",
+                "accession_stoichiometry": "P43772_12",
+            },
+        ]
+        self.assertEqual(expected_result, self.gan.molecule_components["1"])
+
+
