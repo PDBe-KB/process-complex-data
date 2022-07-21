33f73e62d74270369bb30975cc26f5f3142f6bfc
Sri Devan Appasamy <sria@ebi.ac.uk>
2022-07-21 17:37:01 +0100

    Did some refactoring

--- complexes/run_process.py
+++ complexes/run_process.py
@@ -1,12 +1,13 @@
 import argparse
 import pandas as pd
-from pathlib import Path
+import os
 import subprocess
 import shlex
+import time
 
 
 def run_complexes(
-    bolt_url, username, password, csv_path, molecule_name_path, molecule_components_path
+    bolt_url, username, password, csv_path, molecule_name_path, molecule_components_path, complex_portal_path
 ):
     """
     Run the processes to aggregate/process complex data and assign
@@ -20,13 +21,13 @@ def run_complexes(
     # First process to aggregate/process complex data
     first_process_cmd = f"python complexes/process_complex.py -b {bolt_url[0]} -u {username[0]} -p {password[0]} -o {csv_path[0]}"  # noqa: B950
     # Second process to assign name to complexes
-    second_process_cmd = f"python complexes/get_complex_name.py -b {bolt_url[0]} -u {username[0]} -p {password[0]} -o {csv_path[0]} -i1 {molecule_name_path[0]} -i2 {molecule_components_path[0]}"  # noqa: B950
+    second_process_cmd = f"python complexes/get_complex_name.py -b {bolt_url[0]} -u {username[0]} -p {password[0]} -o {csv_path[0]} -i1 {molecule_name_path[0]} -i2 {molecule_components_path[0]} -i3 {complex_portal_path[0]}"  # noqa: B950
 
     subprocess.run(shlex.split(first_process_cmd))
-    # subprocess.run(shlex.split(second_process_cmd))
+    subprocess.run(shlex.split(second_process_cmd))
 
 
-def merge_csv_files(filename1="complexes_mapping.csv", filename2="complexes_names.csv"):
+def merge_csv_files(csv_path, filename1="complexes_mapping.csv", filename2="complexes_names.csv"):
     """
     Merge the csv files produced by the proccesses above into a single file
 
@@ -37,12 +38,11 @@ def merge_csv_files(filename1="complexes_mapping.csv", filename2="complexes_name
                                    "complexes_names.csv".
     """
     output_filename = "complexes_master.csv"
-    base_path = Path.cwd()
-    complete_path = base_path.joinpath("complexes").joinpath("output")
-    df1 = pd.read_csv(complete_path.joinpath(filename1))
-    df2 = pd.read_csv(complete_path.joinpath(filename2))
+    df1 = pd.read_csv(os.path.join(csv_path[0], filename1))
+    df2 = pd.read_csv(os.path.join(csv_path[0], filename2))
     merged_df = df1.merge(df2, on="pdb_complex_id")
-    merged_df.to_csv(complete_path.joinpath(output_filename), index=False)
+    merged_df.to_csv(os.path.join(csv_path[0], output_filename), index=False)
+    print("Complexes_master file has been produced")
 
 
 def main():
@@ -90,6 +90,13 @@ def main():
         help="Path to input CSV file containing manually curated complexes components",
     )
 
+    parser.add_argument(
+        "-i3",
+        "--complex-portal-path",
+        required=True,
+        help="Path to Complex Portal ftp site",
+    )
+
     args = parser.parse_args()
 
     bolt_url = (args.bolt_url,)
@@ -98,6 +105,7 @@ def main():
     csv_path = (args.path,)
     molecule_name_path = (args.molecule_name_path,)
     molecule_components_path = (args.molecule_components_path,)
+    complex_portal_path = (args.complex_portal_path,)
 
     run_complexes(
         bolt_url,
@@ -106,9 +114,12 @@ def main():
         csv_path,
         molecule_name_path,
         molecule_components_path,
+        complex_portal_path,
     )
-    # merge_csv_files()
+    merge_csv_files(csv_path)
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    print("Complete processes take: --- %s seconds ---" % (time.time() - start_time))
